c7faa6efb38689566ef1138c93874af61ecc3342
Adrik <AdrikWarren@gmail.com>
2025-07-06 13:22:50 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import App from './App';
 
 test('renders the app with initial elements', () => {
   render(<App />);
-  
+
   expect(screen.getByText(/Income:/i)).toBeInTheDocument();
-  expect(screen.getByText(/Expenses:/i)).toBeInTheDocument();
   expect(screen.getByText(/No expenses recorded/i)).toBeInTheDocument();
+  expect(screen.getByText(/Total Expenses:/i)).toBeInTheDocument();
 });
 
